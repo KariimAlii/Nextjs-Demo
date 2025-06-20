6baa498545e6f8be73d8350a42d9827424db1d3f
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-20 06:06:21 +0300

    Using usePathName

--- src/app/components/main-header/main-header.js
+++ src/app/components/main-header/main-header.js
@@ -1,10 +1,14 @@
-﻿import Link from "next/link";
+﻿'use client';
+
+import Link from "next/link";
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from "next/image";
 import MainHeaderBackground from "@/app/components/main-header/main-header-background";
+import { usePathname }  from "next/navigation";
 
 export default function MainHeader() {
+    const path = usePathname();
     return (
         <>
             <MainHeaderBackground/>
@@ -17,10 +21,10 @@ export default function MainHeader() {
                 <nav className={classes.nav}>
                     <ul>
                         <li>
-                            <Link href="/meals">Browse Meals</Link>
+                            <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link>
                         </li>
                         <li>
-                            <Link href="/community">Community</Link>
+                            <Link href="/community" className={path.startsWith('/community') ? classes.active : undefined}>Community</Link>
                         </li>
                     </ul>
                 </nav>

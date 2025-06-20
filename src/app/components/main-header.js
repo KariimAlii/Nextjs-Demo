1ca2205b34c63cd181c2f3752214af282ce52be4
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-20 05:30:10 +0300

    Added Main Hear Background SubComponent

--- src/app/components/main-header.js
+++ src/app/components/main-header.js
@@ -2,25 +2,30 @@
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from "next/image";
+import MainHeaderBackground from "@/app/components/main-header-background";
 
 export default function MainHeader() {
     return (
-        <heaeder className={classes.header}>
-            <Link className={classes.logo} href="/">
-                <Image src={logoImg.src} alt="a plate with food on it image" priority />
-                Next Level Food
-            </Link>
+        <>
+            <MainHeaderBackground/>
+            <heaeder className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <Image src={logoImg} alt="a plate with food on it image" priority/>
+                    Next Level Food
+                </Link>
 
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </heaeder>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </heaeder>
+        </>
+
     )
 }

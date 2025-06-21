580ade0cec5b6abeee391acf33a52e1b2d8d5cbb
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-21 03:50:17 +0300

    Added Server Side Data Fetching

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -1,42 +1,24 @@
-﻿'use client';
-import {useEffect, useState} from "react";
-import NewsGrid from "@/app/components/news/news-grid/news-grid";
+﻿import NewsGrid from "@/app/components/news/news-grid/news-grid";
 
-export default function News() {
-    const [news, setNews] = useState(null);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    useEffect(() => {
-        async function fetchNews() {
-            setIsLoading(true);
-            const response = await fetch('http://localhost:8080/news');
-            if(!response.ok) {
-                setIsLoading(false);
-                setError('Failed to fetch News!')
-            }
-            const data = await response.json();
-            console.log(data)
-            setNews(data);
-            setIsLoading(false);
-        }
-        fetchNews();
-    }, [])
-
-    if(error) {
-        return <p>Error: {error}</p>;
-    }
-    if(isLoading) {
-        return <p>Loading...</p>;
+export default async function News() {
+    const response = await fetch('http://localhost:8080/news');
+    if(!response.ok) {
+        throw new Error('Failed to fetch News!')
     }
 
-    let newsContent;
-    if(news) {
-        newsContent = <NewsGrid newsList={news} />
-    }
+    const news = await response.json();
+
+    // if(error) {
+    //     return <p>Error: {error}</p>;
+    // }
+    // if(isLoading) {
+    //     return <p>Loading...</p>;
+    // }
+
     return (
         <>
             <h1>News Page</h1>
-            {newsContent}
+            <NewsGrid newsList={news} />
         </>
 
     );

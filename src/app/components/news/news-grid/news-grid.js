f2c8121b67de968e5709b91cf5cab56213809331
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-21 03:46:33 +0300

    Added Client Side Data Fetching

---
+++ src/app/components/news/news-grid/news-grid.js
@@ -0,0 +1,15 @@
+﻿import classes from './news-grid.module.css';
+import NewsItem from "@/app/components/news/news-item/news-item";
+
+export default function NewsGrid({newsList}) {
+    return (
+        <ul className={classes.newsGridContainer}>
+            {newsList.map(news => (
+                <li key={news._id}>
+                    <NewsItem {...news}/>
+                </li>
+            ))}
+        </ul>
+
+    );
+}
---
+++ src/app/components/news/news-item/news-item.js
@@ -0,0 +1,24 @@
+﻿import classes from './news-item.module.css'
+import Image from "next/image";
+import Link from "next/link";
+export default function NewsItem({ _id, title, slug, image, date, content }) {
+    return (
+        <article className={classes.news}>
+            <header>
+                <div className={classes.image}>
+                    {/*<Image src={image} alt={title} fill/>*/}
+                </div>
+                <div className={classes.headerText}>
+                    <h2>{title}</h2>
+                    <p>{content}</p>
+                </div>
+            </header>
+            <div className={classes.content}>
+                <p className={classes.summary}>{date}</p>
+                <div className={classes.actions}>
+                    <Link href={`/meals/${slug}`}>View Details</Link>
+                </div>
+            </div>
+        </article>
+    );
+}

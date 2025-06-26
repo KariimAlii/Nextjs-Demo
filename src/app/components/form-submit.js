b769fa232b738d768f4e1e97710a17297ef85cb7
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-27 02:32:29 +0300

    Form Submit Component

---
+++ src/app/components/form-submit.js
@@ -0,0 +1,18 @@
+﻿"use client";
+
+import {useFormStatus} from "react-dom";
+
+export default function FormSubmit(params) {
+    const status = useFormStatus();
+    if(status.pending) {
+        return <p>Creating a post....</p>
+    }
+    // gives you a status object which represents the status of the sorrounding form
+    // must be used between <form> tags
+    return (
+        <>
+            <button type="reset">Reset</button>
+            <button>Create Post</button>
+        </>
+    )
+}

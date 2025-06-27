f0b18e39ae9aa1f1c46fa33355619a5ec8b10ec0
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-06-27 06:06:20 +0300

    Added Group Auth Layout for pages that needs authentication

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,22 @@
+﻿import '../globals.css';
+import MainHeader from "@/app/components/main-header/main-header";
+import MainHeaderBackground from "@/app/components/main-header/main-header-background";
+
+export const metadata = {
+    title: 'NextLevel Food',
+    description: 'Delicious meals, shared by a food-loving community.',
+};
+
+export default function AuthRootLayout({ children }) {
+    return (
+        <>
+            <header>
+                <h3>Welcome Back</h3>
+            </header>
+            <form>
+                <button>Logout</button>
+            </form>
+            {children}
+        </>
+    );
+}
